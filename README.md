7885f8d1af4a166def38321b02aa5212fb944180
OlaJFrick <info@culturehero.org>
2017-01-30 21:32:56 +0100

    Initial Environment set-up

---
+++ README.md
@@ -0,0 +1,15 @@
+# my vinyl blog
+A simple excersis in JavaScript, Bootstrap, Less & HTML using JSON as post storage.
+
+## Installation
+Clone or download the source files.
+
+Install dependencies:
+```sh
+$ npm install
+```
+Run the app:
+```sh
+$ node app.js
+```
+Go to http://localhost:3000
---
+++ www/js/main.js
@@ -0,0 +1,58 @@
+// function addFamilyName() {
+//     $('.test-button').click(function(){
+//         var get = $('#last-name').val();
+//         $('.test-p').text(get);
+//         // famLastName.push(get);
+
+//     });
+// }
+
+
+
+
+
+
+//     $(function() {
+
+//     displayHiddenSongName();
+
+//     // alert('hey');
+
+//     // var alphabet = "abcdefghijklmnopqrstuvwxyz";
+    
+//     // function addName(butNames, alph, aLength) {
+//     //     console.log(butNames + " " + alph + " " + aLength);
+//     //     var humbug = (butNames + " " + alph + " " + aLength);
+//     //     return humbug;
+//     // }
+
+//     // var sum = addName(buttonNames[1], alphabet[1], alphabet.length);
+
+
+
+
+    // $('.test-button').click(function(){
+    //     var firstname = $('#first-name').val();
+    //     $('.test-p').text(firstname);
+    // });
+
+
+    
+
+
+
+//     function displayHiddenSongName() {
+//     	// Hide Song info
+//         $('.display-music-info').hide();
+
+//         // Display Song info
+//         $(".display-music-icon").click(function() {
+//             $('.display-music-info').show(function() {
+//                 $('.display-music-info').fadeOut(8000);
+//             });
+//         });
+//     }
+
+
+// });
+
